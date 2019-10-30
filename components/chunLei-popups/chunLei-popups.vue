90d7137930ce493994ba65964eb74f5055ced964
15157757001 <a949600520@qq.com>
2019-10-30 22:56:02 +0800

    u

---
+++ components/chunLei-popups/chunLei-popups.vue
@@ -0,0 +1,316 @@
+<template>
+	<view class="mask" :class="!show?'':'mask-show'" :style="{backgroundColor:show?maskBg:'background:rgba(0,0,0,0)'}" @tap="tapMask">
+		<view class="popups" :style="{top: popupsTop ,left: popupsLeft,flexDirection:direction}" :class="theme">
+			<text :class="dynPlace" :style="{width:'0px',height:'0px'}"></text>
+			<view v-for="(item,index) in popData" :key="index" @tap.stop="tapItem(item)" class="itemChild" :class="direction=='row'?'solid-right':'solid-bottom'">
+				<image :src="item.icon" v-if="item.icon"></image>{{item.title}}
+			</view>
+			<slot></slot>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			maskBg:{
+				type:String,
+				default:'rgba(0,0,0,0)'
+			},
+			placement:{
+				type:String,
+				default:'top-start' //top-start top-end bottom-start bottom-end 
+			},
+			direction:{
+				type:String,
+				default:'column' //column row
+			},
+			x:{
+				type:Number,
+				default:0
+			},
+			y:{
+				type:Number,
+				default:0
+			},
+			value:{
+				type:Boolean,
+				default:false
+			},
+			popData:{
+				type:Array,
+				default:()=>[]
+			},
+			theme:{
+				type:String,
+				default:'light' //light dark
+			},
+			dynamic:{
+				type:Boolean,
+				default:false
+			},
+			gap:{
+				type:Number,
+				default:20
+			}
+		},
+		data(){
+			return{
+				popupsTop:'0px',
+				popupsLeft:'0px',
+				show:false,
+				dynPlace:''
+			}
+		},
+		mounted() {
+			this.popupsPosition()
+		},
+		methods:{
+			tapMask(){
+				
+				this.$emit('input',!this.value)
+			},
+			tapItem(item){
+				this.$emit('tapPopup',item)
+				this.$emit('input',!this.value)
+			},
+			async popupsPosition(){
+				let promise = new Promise((resolve,reject)=>{
+					uni.getSystemInfo({
+						success: function(e) {
+							let customBar
+							// #ifndef MP
+							if (e.platform == 'android') {
+								customBar = e.statusBarHeight + 50;
+							} else {
+								customBar = e.statusBarHeight + 45;
+							};
+							// #endif
+							resolve(customBar)
+						}
+					})
+				})
+				let statusBar = await promise
+				//statusBar = 0
+				let popupsDom = uni.createSelectorQuery().in(this).select(".popups")
+				popupsDom.fields({
+				    size: true,  
+				}, (data) => {
+					let width = data.width
+					let height = data.height
+					let y = this.dynamic?this.dynamicGetY(this.y,this.gap):this.transformRpx(this.y)
+					
+					let x = this.dynamic?this.dynamicGetX(this.x,this.gap):this.transformRpx(this.x)
+					
+					// #ifdef H5
+					y = this.dynamic?(this.y+statusBar): this.transformRpx(this.y+statusBar)
+					// #endif  
+					this.dynPlace = this.dynamic?this.getPlacement(x,y):this.placement
+					
+					switch(this.dynPlace){
+						case 'top-start':
+							this.popupsTop = `${y+9}px`
+							this.popupsLeft = `${x-15}px`
+							break;
+						case 'top-end':
+							this.popupsTop = `${y+9}px`
+							this.popupsLeft = `${x+15-width}px`
+							break;
+						case 'bottom-start':
+							this.popupsTop = `${y-18-height}px`
+							this.popupsLeft = `${x-15}px`
+							break;
+						case 'bottom-end':
+							this.popupsTop = `${y-9-height}px`
+							this.popupsLeft = `${x+15-width}px`
+							break;
+					}
+				}).exec();
+			},
+			getPlacement(x,y){
+				let width = uni.getSystemInfoSync().windowWidth
+				let height = uni.getSystemInfoSync().windowHeight
+				if(x>width/2&&y>height/2){
+					return 'bottom-end'
+				}else if(x<width/2&&y<height/2){
+					return 'top-start'
+				}else if(x>width/2&&y<height/2){
+					return 'top-end'
+				}else if(x<width/2&&y>height/2){
+					return 'bottom-start'
+				}else if(x>width/2){
+					return 'top-end'
+				}else{
+					return 'top-start'
+				}
+			},
+			dynamicGetY(y,gap){
+				
+				let height = uni.getSystemInfoSync().windowHeight
+				y = y<gap?gap:y
+				y = height - y <gap? (height - gap) : y
+				
+				return y
+			},
+			dynamicGetX(x,gap){
+				let width = uni.getSystemInfoSync().windowWidth
+				x = x< gap?gap:x
+				x = width - x <gap? (width - gap) : x
+				return x
+			},
+			transformRpx(params){
+				
+				return params*uni.getSystemInfoSync().screenWidth/375
+			}
+		},
+		watch:{
+			value:{
+				immediate:true,
+				handler(newVal,oldVal){
+					if(newVal) this.popupsPosition()
+					if(!this.dynamic){
+						this.show = newVal
+					}else{
+						setTimeout(()=>{
+							this.show = newVal
+						},100)
+					}
+				}
+			},
+			placement:{
+				immediate:true,
+				handler(newVal,oldVal){
+					this.dynPlace = newVal
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.mask{
+		position: fixed;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: 9999;
+		transition: background 0.3s ease-in-out;
+		visibility: hidden;
+		
+		&.mask-show{
+			
+			visibility: visible;
+		}
+	}
+	.popups{
+		position: absolute;
+		padding: 20rpx;
+		border-radius: 5px;
+		display:flex;
+		view{
+			padding: 10rpx;
+		}
+		image{
+			display: inline-block;
+			vertical-align: middle;
+			width: 40rpx;
+			height: 40rpx;
+			margin-right: 20rpx;
+		}
+	}
+	.dark{
+		background-color: rgba(0, 0, 0, 0.8);
+		color: #fff;
+		.top-start:after {
+			content: "";
+			position: absolute;
+			top: -18rpx;
+			left: 10rpx;
+			border-width: 0 20rpx 20rpx;
+			border-style: solid;
+			border-color: transparent transparent rgba(0, 0, 0, 0.8);
+		}
+		.top-end:after {
+			content: "";
+			position: absolute;
+			top: -18rpx;
+			right: 10rpx;
+			border-width: 0 20rpx 20rpx;
+			border-style: solid;
+			border-color: transparent transparent rgba(0, 0, 0, 0.8);
+		}
+		.bottom-start:after {
+			content: "";
+			position: absolute;
+			bottom: -18rpx;
+			left: 10rpx;
+			border-width: 20rpx 20rpx 0 ;
+			border-style: solid;
+			border-color: rgba(0, 0, 0, 0.8) transparent transparent ;
+			
+		}
+		.bottom-end:after {
+			content: "";
+			position: absolute;
+			bottom: -18rpx;
+			right: 10rpx;
+			border-width: 20rpx 20rpx 0 ;
+			border-style: solid;
+			border-color: rgba(0, 0, 0, 0.8) transparent transparent ;
+		}
+	}
+	.light{
+		
+		box-shadow: 0upx 0upx 30upx rgba(0,0,0,0.2);
+		background: #fff;
+		.top-start:after {
+			content: "";
+			position: absolute;
+			top: -18rpx;
+			left: 10rpx;
+			border-width: 0 20rpx 20rpx;
+			border-style: solid;
+			border-color: transparent transparent #fff;
+		}
+		.top-end:after {
+			content: "";
+			position: absolute;
+			top: -18rpx;
+			right: 10rpx;
+			border-width: 0 20rpx 20rpx;
+			border-style: solid;
+			border-color: transparent transparent #fff;
+		}
+		.bottom-start:after {
+			content: "";
+			position: absolute;
+			bottom: -18rpx;
+			left: 10rpx;
+			border-width: 20rpx 20rpx 0 ;
+			border-style: solid;
+			border-color: #fff transparent transparent ;
+			
+		}
+		.bottom-end:after {
+			content: "";
+			position: absolute;
+			bottom: -18rpx;
+			right: 10rpx;
+			border-width: 20rpx 20rpx 0 ;
+			border-style: solid;
+			border-color: #fff transparent transparent ;
+		}
+	}
+	.solid-bottom{
+		border-bottom: 1px solid #ccc;
+	}
+	.solid-right{
+		
+		border-right: 1px solid #ccc;
+	}
+	.popups .itemChild:last-child{
+		border: none;
+	}
+	
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,136 @@
+<template>
+	<view class="content">
+		<view class="light">
+			<text @tap="taptext($event,1)">动态获取</text>
+			<text @tap="value1=!value1">白色纵向</text>
+		</view>
+		<text>黑色横向</text>
+		<view class="dark">
+			<view>
+				<text @tap="value2=!value2">top-start</text>
+				
+				<text @tap="value3=!value3">top-end</text>
+			</view>
+			<view>
+				<text @tap="value4=!value4">bottom-start</text>
+				<text @tap="value5=!value5">bottom-end</text>
+			</view>
+		</view>
+		<chunLei-popups v-model="value0" :x="x" :y="y" dynamic>
+			<view>插槽</view>
+		</chunLei-popups>
+		<chunLei-popups v-model="value1" :popData="data1" @tapPopup="tapPopup" :x="344" :y="20" placement="top-end">
+			
+		</chunLei-popups>
+		<chunLei-popups v-model="value2" :popData="data2" @tapPopup="tapPopup" :x="66" :y="170" direction="row" theme="dark" >
+			
+		</chunLei-popups>
+		<chunLei-popups v-model="value3" :popData="data2" @tapPopup="tapPopup" :x="303" :y="170" direction="row" theme="dark" placement="top-end">
+			
+		</chunLei-popups>
+		<chunLei-popups v-model="value4" :popData="data2" @tapPopup="tapPopup" :x="84" :y="430" direction="row" theme="dark" placement="bottom-start">
+			
+		</chunLei-popups>
+		<chunLei-popups v-model="value5" :popData="data2" @tapPopup="tapPopup" :x="302" :y="430" direction="row" theme="dark" placement="bottom-end">
+			
+		</chunLei-popups>
+		<view class="light">
+			<text @tap="taptext($event,1)">动态获取</text>
+			<text @tap="taptext($event,1)">动态获取</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	import chunLeiPopups from "@/components/chunLei-popups/chunLei-popups.vue";
+	export default {
+		components:{
+			chunLeiPopups
+		},
+		data() {
+			return {
+				value0:false,
+				value1:false,
+				value2:false,
+				value3:false,
+				value4:false,
+				value5:false,
+				x:0,
+				y:0,
+				data1:[
+					{
+						title:'创建群聊',
+						icon:'../../static/chuangjianqunliao-lan.png'
+					},
+					{
+						title:'加好友/群',
+						icon:'../../static/tianjiahaoyou.png'
+					},
+					{
+						title:'扫一扫',
+						icon:'../../static/scan_icon.png'
+					},
+					{
+						title:'面对面快传',
+						icon:'../../static/zhifeiji.png'
+					},
+					{
+						title:'收付款',
+						icon:'../../static/shoufukuan.png'
+					}
+				],
+				data2:[{title:'复制'},{title:'转发'},{title:'回复'},{title:'删除'}]
+			}
+		},
+		onLoad() {
+
+		},
+		mounted() {
+		
+		},
+		methods: {
+			tapPopup(e){
+				uni.showToast({
+					title:e.title,
+					icon:'none'
+				})
+			},
+			taptext(e,index){
+				console.log(e,index)
+				this.x = e.touches[0].clientX
+				this.y = e.touches[0].clientY
+				this.value0 = !this.value0
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		font-size:16px;
+		.light{
+			display: flex;
+			width: 100%;
+			justify-content: space-between;
+			margin-bottom:150rpx;
+		}
+		.dark{
+			margin-bottom:150rpx;
+			padding: 50rpx;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			height: 600rpx;
+			width: 80%;
+			view{
+				display: flex;
+				justify-content: space-between;
+			}
+		}
+	}
+
+</style>
